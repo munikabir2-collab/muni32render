29bdc2ef343eda5f42f1820e8e9ee8155fe510d6
Munilal Tanti <munikabir2@gmail.com>
2026-07-15 14:16:54 +0530

    update argon2 auth and python runtime

--- auth.py
+++ auth.py
@@ -5,13 +5,15 @@ pwd_context = CryptContext(
     deprecated="auto"
 )
 
+
 def hash_password(password: str):
     password = password.strip()
 
-    if len(password.encode("utf-8")) > 72:
-        raise ValueError("Password too long (max 72 bytes)")
-
     return pwd_context.hash(password)
 
+
 def verify_password(plain: str, hashed: str):
-    return pwd_context.verify(plain.strip(), hashed)
+    return pwd_context.verify(
+        plain.strip(),
+        hashed
+    )
